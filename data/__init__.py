baa0dd8b1e24a13d81948fd7f2367e254b1996b0
Karl Knechtel <karl.a.knechtel@gmail.com>
2023-04-13 09:19:35 -0400

    rm redundant checks

--- data/__init__.py
+++ data/__init__.py
@@ -213,69 +213,68 @@ class Field(Structure, Enum):
     # size, format code, endian
     # names alluding to underlying format codes
     # integers
-    b = (1, 'b', '|')
-    B = (1, 'B', '|')
-    h = (2, 'h', '|')
-    H = (2, 'H', '|')
+    b = ('b', '|')
+    B = ('B', '|')
+    h = ('h', '|')
+    H = ('H', '|')
     # Normalize to always use the same code internally.
-    i = (4, 'i', '|')
-    I = (4, 'I', '|')
-    l = (4, 'i', '|')
-    L = (4, 'I', '|')
-    q = (8, 'q', '|')
-    Q = (8, 'Q', '|')
+    i = ('i', '|')
+    I = ('I', '|')
+    l = ('i', '|')
+    L = ('I', '|')
+    q = ('q', '|')
+    Q = ('Q', '|')
     # booleans
-    _ = (1, '?', '|')
+    _ = ('?', '|')
     # floats
-    e = (2, 'e', '|')
-    f = (4, 'f', '|')
-    d = (8, 'd', '|')
+    e = ('e', '|')
+    f = ('f', '|')
+    d = ('d', '|')
     # text
-    x = (1, 'x', '|') # a byte whose value will be discarded.
-    s = (1, 's', '|') # "arrays" of bytes will become a single `bytes` object.
-    c = (1, 's', '|') # a single byte, either way.
+    x = ('x', '|') # a byte whose value will be discarded.
+    s = ('s', '|') # "arrays" of bytes will become a single `bytes` object.
+    c = ('s', '|') # a single byte, either way.
     # deliberately not supporting "Pascal strings".
     # The "native pointer" type also doesn't have a standard size.
     # names directly indicating type properties
     # Integers
-    s1 = (1, 'b', '<')
-    S1 = (1, 'b', '>')
-    u1 = (1, 'B', '<')
-    U1 = (1, 'B', '>')
-    s2 = (2, 'h', '<')
-    S2 = (2, 'h', '>')
-    u2 = (2, 'H', '<')
-    U2 = (2, 'H', '>')
-    s4 = (4, 'i', '<')
-    S4 = (4, 'i', '>')
-    u4 = (4, 'I', '<')
-    U4 = (4, 'I', '>')
-    s8 = (8, 'q', '<')
-    S8 = (8, 'q', '>')
-    u8 = (8, 'Q', '<')
-    U8 = (8, 'Q', '>')
+    s1 = ('b', '<')
+    S1 = ('b', '>')
+    u1 = ('B', '<')
+    U1 = ('B', '>')
+    s2 = ('h', '<')
+    S2 = ('h', '>')
+    u2 = ('H', '<')
+    U2 = ('H', '>')
+    s4 = ('i', '<')
+    S4 = ('i', '>')
+    u4 = ('I', '<')
+    U4 = ('I', '>')
+    s8 = ('q', '<')
+    S8 = ('q', '>')
+    u8 = ('Q', '<')
+    U8 = ('Q', '>')
     # For non-numeric types, no endianness applies inherently, but these types
     # will imply an endianness for the overall struct/array containing them.
     # booleans
-    b1 = (1, '?', '<')
-    B1 = (1, '?', '>')
+    b1 = ('?', '<')
+    B1 = ('?', '>')
     # floats
-    f2 = (2, 'e', '<')
-    F2 = (2, 'e', '>')
-    f4 = (4, 'f', '<')
-    F4 = (4, 'f', '>')
-    f8 = (8, 'd', '<')
-    F8 = (8, 'd', '>')
+    f2 = ('e', '<')
+    F2 = ('e', '>')
+    f4 = ('f', '<')
+    F4 = ('f', '>')
+    f8 = ('d', '<')
+    F8 = ('d', '>')
     # text
-    p1 = (1, 'x', '<')
-    P1 = (1, 'x', '>')
-    t1 = (1, 's', '<')
-    T1 = (1, 's', '>')
+    p1 = ('x', '<')
+    P1 = ('x', '>')
+    t1 = ('s', '<')
+    T1 = ('s', '>')
 
 
-    def __init__(self, size, code, endian):
+    def __init__(self, code, endian):
         super().__init__(Atom(code), endian)
-        assert self.size == size
 
 
 globals().update(Field.__members__)
